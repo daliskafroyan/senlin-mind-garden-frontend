6ce9705fbf9e740dafc8364ebdc64d67c338b106
daliskafroyan <firstaroyand.paper@gmail.com>
2022-03-01 14:29:45 +0700

    style: adding prettier

--- components/organisms/TimelineCard/TimelineCard.tsx
+++ components/organisms/TimelineCard/TimelineCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import {
     Timeline,
     Text,
@@ -7,28 +7,25 @@ import {
     ColorSwatch,
     Card,
     Accordion,
-    createStyles
-} from "@mantine/core";
-
-
-
+    createStyles,
+} from '@mantine/core';
 
 const useStyles = createStyles((_theme, _params, getRef) => {
-    const controlRef = getRef('control')
+    const controlRef = getRef('control');
     return {
-        control: { 
+        control: {
             ref: controlRef,
             border: 0,
             opacity: 0.6,
             color: 'black',
 
-            '&:hover' : {
+            '&:hover': {
                 backgroundColor: 'transparent',
                 opacity: 1,
-            }
-        }
-    }
-})
+            },
+        },
+    };
+});
 
 const TimelineCard = ({
     timelineTitle,
@@ -38,45 +35,45 @@ const TimelineCard = ({
     author,
     location,
 }) => {
-    const {classes} = useStyles();
+    const { classes } = useStyles();
     return (
         <Timeline active={1} bulletSize={24} lineWidth={2}>
             <Timeline.Item title={timelineTitle}>
-                <Card padding="md" withBorder style={{ marginTop: "0.75rem" }}>
+                <Card padding='md' withBorder style={{ marginTop: '0.75rem' }}>
                     <Box>
                         <ColorSwatch
-                            color={"red"}
+                            color={'red'}
                             size={10}
-                            style={{ marginBottom: "0.2rem" }}
+                            style={{ marginBottom: '0.2rem' }}
                         />
-                        <Text weight={"bold"} size={"xl"}>
+                        <Text weight={'bold'} size={'xl'}>
                             {cardTitle}
                         </Text>
                         <Badge
-                            color="red"
-                            radius="xs"
-                            variant="filled"
-                            size="sm"
+                            color='red'
+                            radius='xs'
+                            variant='filled'
+                            size='sm'
                         >
                             {mainTopic}
                         </Badge>
                         <Box
                             style={{
-                                display: "inline-block",
-                                marginLeft: "0.5rem",
+                                display: 'inline-block',
+                                marginLeft: '0.5rem',
                             }}
                         >
-                            <Text color="dimmed" size="sm">
-                                of{" "}
-                                <Text variant="link" component="span" inherit>
+                            <Text color='dimmed' size='sm'>
+                                of{' '}
+                                <Text variant='link' component='span' inherit>
                                     {specificTopic}
-                                </Text>{" "}
-                                from{" "}
-                                <Text variant="link" component="span" inherit>
+                                </Text>{' '}
+                                from{' '}
+                                <Text variant='link' component='span' inherit>
                                     {location}
-                                </Text>{" "}
-                                by{" "}
-                                <Text variant="link" component="span" inherit>
+                                </Text>{' '}
+                                by{' '}
+                                <Text variant='link' component='span' inherit>
                                     {author}
                                 </Text>
                             </Text>
@@ -84,22 +81,22 @@ const TimelineCard = ({
                     </Box>
                     <Accordion
                         classNames={classes}
-                        style={{ marginTop: "1rem" }}
+                        style={{ marginTop: '1rem' }}
                         styles={{
                             item: { border: 0 },
                             itemOpened: { border: 0 },
                             control: { padding: 0 },
                             contentInner: { padding: 0 },
                         }}
-                        iconPosition="right"
+                        iconPosition='right'
                         multiple
                     >
-                        <Accordion.Item label="Customization">
-                            <Text color="" size="sm">
-                                You&apos;ve created new branch{" "}
-                                <Text variant="link" component="span" inherit>
+                        <Accordion.Item label='Customization'>
+                            <Text color='' size='sm'>
+                                You&apos;ve created new branch{' '}
+                                <Text variant='link' component='span' inherit>
                                     fix-notifications
-                                </Text>{" "}
+                                </Text>{' '}
                                 from master
                             </Text>
                         </Accordion.Item>
